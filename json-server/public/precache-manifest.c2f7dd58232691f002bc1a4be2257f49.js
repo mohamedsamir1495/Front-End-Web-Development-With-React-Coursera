fbb8fe0ce5ebafabc5c987aeceb8ba8f519b1635
Mohamed Samir <mohamed2.samir.ext@orange.com>
2020-10-04 23:05:55 +0200

    Final Commit for the course :)

---
+++ json-server/public/precache-manifest.c2f7dd58232691f002bc1a4be2257f49.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78410d820ba90c962479412ae9729492",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc35a2687ebe52b337b",
+    "url": "/static/css/2.12cfaff4.chunk.css"
+  },
+  {
+    "revision": "4c406999d22d1acd5a0b",
+    "url": "/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "4bc35a2687ebe52b337b",
+    "url": "/static/js/2.eaa3063c.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.eaa3063c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c406999d22d1acd5a0b",
+    "url": "/static/js/main.16ac5215.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
